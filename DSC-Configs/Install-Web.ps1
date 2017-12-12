d2fad06e3faaedf1d4fc724a3ada2c032aa02c09
Ashish Raj Srivastava <ashishrajsrivastava@yahoo.in>
2017-12-13 02:53:26 +0530

    config updated

---
+++ DSC-Configs/Install-Web.ps1
@@ -0,0 +1,28 @@
+﻿configuration Install-Web
+{
+param([string[]]$ComputerName = $env:COMPUTERNAME)
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node $ComputerName
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        WindowsFeature IIS
+        {
+           Ensure = "Present"
+           Name   = "Web-Server"
+        }
+
+        WindowsFeature ASP
+        {
+           Ensure = "Present"
+           Name   = "Web-Asp-Net45"
+        }
+        
+        WindowsFeature ADPS
+        {
+           Ensure = "Present"
+           Name   = "RSAT-AD-PowerShell"
+        }   
+    }
+}
